7f425c0067ea7175961f5dd6d30f3f4fcbcd8b79
UILabel <1172242456@qq.com>
2016-06-13 14:40:24 +0800

    限制长度为且两位小数
    限制长度为且两位小数

---
+++ UITextField_LimitLengthDemo/ViewController.m
@@ -0,0 +1,134 @@
+//
+//  ViewController.m
+//  UITextField_LimitLengthDemo
+//
+//  Created by admin on 16/6/13.
+//  Copyright © 2016年 AlezJi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()<UITextFieldDelegate>
+@property (nonatomic,strong)UITextField *NumTextField;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    self.NumTextField = [[UITextField alloc]initWithFrame:CGRectMake(20, 100, 335, 40)];
+    self.NumTextField.delegate = self;
+    self.NumTextField.placeholder = @"限制长度为8且两位小数";
+    self.NumTextField.borderStyle = UITextBorderStyleRoundedRect;
+    self.NumTextField.font = [UIFont systemFontOfSize:15];
+    self.NumTextField.keyboardType = UIKeyboardTypeDecimalPad;
+    [self.view addSubview:self.NumTextField];
+
+    
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(textFiledEditChanged:) name:@"UITextFieldTextDidChangeNotification" object:self.NumTextField];
+    
+}
+//限制 8位
+-(void)textFiledEditChanged:(NSNotification *)obj{
+    UITextField *textField = (UITextField *)obj.object;
+    
+    NSString *toBeString = textField.text;
+    NSString *lang = [[UIApplication sharedApplication]textInputMode].primaryLanguage; // 键盘输入模式
+    
+    if (textField == self.NumTextField) {
+        if ([lang isEqualToString:@"zh-Hans"]) { // 简体中文输入，包括简体拼音，健体五笔，简体手写
+            UITextRange *selectedRange = [textField markedTextRange];
+            //获取高亮部分
+            UITextPosition *position = [textField positionFromPosition:selectedRange.start offset:0];
+            // 没有高亮选择的字，则对已输入的文字进行字数统计和限制
+            if (!position) {
+                if (toBeString.length > 16) {
+                    textField.text = [toBeString substringToIndex:16];
+                }
+            }
+            // 有高亮选择的字符串，则暂不对文字进行统计和限制
+            else{
+                
+            }
+        }
+        // 中文输入法以外的直接对其统计限制即可，不考虑其他语种情况
+        else{
+            if (toBeString.length > 8) {
+                textField.text = [toBeString substringToIndex:16];
+            }
+        }
+    }
+    
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    if (textField== self.NumTextField) {
+        BOOL isHaveDian = NO;
+        if ([textField.text rangeOfString:@"."].location == NSNotFound) {
+            isHaveDian = NO;
+        }else{
+            isHaveDian = YES;
+        }
+        if ([string length] > 0) {
+            
+            unichar single = [string characterAtIndex:0];//当前输入的字符
+            if ((single >= '0' && single <= '9') || single == '.') {//数据格式正确
+                
+                //首字母不能为0和小数点
+                if([textField.text length] == 0){
+                    if(single == '.') {
+                        
+                        return NO;
+                    }
+                }
+                
+                //输入的字符是否是小数点
+                if (single == '.') {
+                    if(!isHaveDian)//text中还没有小数点
+                    {
+                        isHaveDian = YES;
+                        return YES;
+                        
+                    }else{
+                        
+                        return NO;
+                    }
+                }else{
+                    if (isHaveDian) {//存在小数点
+                        //判断小数点的位数
+                        NSRange ran = [textField.text rangeOfString:@"."];
+                        if (range.location - ran.location <= 2) {
+                            return YES;
+                        }else{
+                            
+                            return NO;
+                        }
+                    }else{
+                        return YES;
+                    }
+                }
+            }else{//输入的数据格式不正确
+                
+                return NO;
+            }
+        }else{
+            return YES;
+        }
+
+    }
+    return NO;
+
+}
+
+-(void)dealloc{
+    [[NSNotificationCenter defaultCenter]removeObserver:self name:@"UITextFieldTextDidChangeNotification" object:self.NumTextField];
+
+}
+
+
+@end
